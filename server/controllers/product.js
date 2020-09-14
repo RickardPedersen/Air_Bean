06065ef2e5fa4fcce2c08305b1e43a6c078b4506
Rickard Pedersen <pedersen.rickard@outlook.com>
2020-09-14 16:17:11 +0200

    bug fix

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -4,7 +4,7 @@ module.exports = {
 
     async newProduct (req, res) {
         const {title, desc, price} = req.body;
-        let product = await createProduct(title, desc, price);
+        const product = await createProduct(title, desc, price);
         res.status(201).json(product);
     }
 
--- server/models/product.js
+++ server/models/product.js
@@ -2,11 +2,11 @@ const {product} = require('../database/db');
 
 module.exports = {
     async createProduct(title, desc, price) {
-        const productObject = (
+        const productObject = {
             title,
             desc,
             price
-        )
+        }
         try {
             return await product.create(productObject);
         } catch (error) {
